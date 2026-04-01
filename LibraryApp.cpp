a9575c0b7cf03f0869975577c5b71e58ce5c5066
maxmullerbueno <max.albieri@outlook.com>
2026-04-01 20:19:39 +1100

    Initialize Library Management app structure
    This code sets up the main structure for a Library Management application, including a welcome message. The previous menu and calculator functionality have been commented out for future implementation.

--- LibraryApp.cpp
+++ LibraryApp.cpp
@@ -5,5 +5,78 @@ using namespace std;
 
 int main()
 {
+// Example from class to be based on:
+cout << "==========================" << endl;
+cout << "=   Library Management   =" << endl;
+cout << "==========================" << endl;
 
+//int option;
+  //while (true) {
+       // cout << "=      Menu      =" << endl;
+      //  cout << "=      1 Add     =" << endl;
+      //  cout << "=   2 Subtract   =" << endl;
+      //  cout << "=   3 Multiply   =" << endl;
+      //  cout << "=    4 Divide    =" << endl;
+      //  cout << "=     5 Exit     =" << endl;
+      //  cout << "==================" << endl;
+      //  cout << "= Select an Option (1-5): =" << endl;
+      //  cin >> option;
+        //cout << " You have selected the Option: " << option << endl;
+        // Exit option
+      //  if (option == 5) {
+       //     break;
+       //     cout << "ABC" << endl; //Dead code
+       // }
+        // Add 2 numbers
+      //  if (option == 1) {
+       //     Calculator myCalc;
+         //   int number1, number2;
+           // //ask for the first operand
+       //     cout << "input the 1st number: ";
+        //    cin >> number1;
+        //    //ask for the second operand
+        //    cout << "input the 2nd number: ";
+        //    cin >> number2;
+        //    double result = myCalc.add(number1, number2);
+        //    cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
+       // }
+       // if (option == 2) {
+       //     Calculator myCalc;
+         //   int number1, number2;
+            //ask for the first operand
+         //   cout << "input the 1st number: ";
+           // cin >> number1;
+            //ask for the second operand
+           // cout << "input the 2nd number: ";
+            //cin >> number2;
+            //double result = myCalc.subtract(number1, number2);
+            //cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
+       // }
+       // if (option == 3) {
+         //   Calculator myCalc;
+           // int number1, number2;
+            //ask for the first operand
+           // cout << "input the 1st number: ";
+         //   cin >> number1;
+            //ask for the second operand
+        //    cout << "input the 2nd number: ";
+         //   cin >> number2;
+        //    double result = myCalc.multiply(number1, number2);
+         //   cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
+        }
+      //  if (option == 4) {
+       //     Calculator myCalc;
+       //     int number1, number2;
+            //ask for the first operand
+        //    cout << "input the 1st number: ";
+       //     cin >> number1;
+            //ask for the second operand
+      //      cout << "input the 2nd number: ";
+       //     cin >> number2;
+      //      double result = myCalc.divide(number1, number2);
+       //     cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
+   //     }
+  //  }
+  //  cout << "Thank you, see you next time! :) " << endl;
+}
   
