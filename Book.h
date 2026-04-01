ca5b99f8660af8e0548667b31aff537545218b4a
maxmullerbueno <max.albieri@outlook.com>
2026-04-01 23:45:30 +1100

    Update Book.h

--- Book.h
+++ Book.h
@@ -2,4 +2,9 @@
 #include <iostream>
 using namespace std;
 
-
+class Book {
+public: // Public or Private? Investigate.
+    string title;
+    string author;
+    string ISBN;
+    bool availability;
