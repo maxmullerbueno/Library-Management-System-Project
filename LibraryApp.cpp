e22862e6d7314875e69ba2d3de00c2771891fcf0
maxmullerbueno <max.albieri@outlook.com>
2026-04-06 16:14:06 +1000

    Update LibraryApp.cpp

--- LibraryApp.cpp
+++ LibraryApp.cpp
@@ -78,62 +78,6 @@ int option;
             cout << "Book not found." << endl;
             }
         }
-
-
-
-
-
-      
-           // //ask for the first operand
-       //     cout << "input the 1st number: ";
-        //    cin >> number1;
-        //    //ask for the second operand
-        //    cout << "input the 2nd number: ";
-        //    cin >> number2;
-        //    double result = myCalc.add(number1, number2);
-        //    cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
-       // }
-
-      
-       // if (option == 2) {
-       //     Calculator myCalc;
-         //   int number1, number2;
-            //ask for the first operand
-         //   cout << "input the 1st number: ";
-           // cin >> number1;
-            //ask for the second operand
-           // cout << "input the 2nd number: ";
-            //cin >> number2;
-            //double result = myCalc.subtract(number1, number2);
-            //cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
-       // }
-
-      
-       // if (option == 3) {
-         //   Calculator myCalc;
-           // int number1, number2;
-            //ask for the first operand
-           // cout << "input the 1st number: ";
-         //   cin >> number1;
-            //ask for the second operand
-        //    cout << "input the 2nd number: ";
-         //   cin >> number2;
-        //    double result = myCalc.multiply(number1, number2);
-         //   cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
-        }
-      //  if (option == 4) {
-       //     Calculator myCalc;
-       //     int number1, number2;
-            //ask for the first operand
-        //    cout << "input the 1st number: ";
-       //     cin >> number1;
-            //ask for the second operand
-      //      cout << "input the 2nd number: ";
-       //     cin >> number2;
-      //      double result = myCalc.divide(number1, number2);
-       //     cout << "The result of adding " << number1 << " and " << number2 << " is " << result << endl;
-   //     }
-  //  }
-  //  cout << "Thank you, see you next time! :) " << endl;
-}
-  
+        cout << "Program finished." << endl;
+        return 0;
+       }
