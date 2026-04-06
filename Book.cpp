e5d0e1d8020cd543a1f81f752686a77c93d1207e
maxmullerbueno <max.albieri@outlook.com>
2026-04-06 14:08:54 +1000

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -3,3 +3,9 @@
 Book::Book() {
     availability = 1; // book starts as available
 }
+void Book::setBookDetails(string t, string a, int id){
+    title = t;
+    author = a;
+    ISBN = id;
+    availability = 1;
+}
