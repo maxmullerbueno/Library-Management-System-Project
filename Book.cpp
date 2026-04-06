27eb9e29ad6959da021575042376a063644b4af5
maxmullerbueno <max.albieri@outlook.com>
2026-04-06 14:31:55 +1000

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -3,9 +3,12 @@
 Book::Book() {
     availability = 1; // book starts as available
 }
+// Set up pointer setBookDetails as stated on Book.h
 void Book::setBookDetails(string t, string a, int id){
     title = t;
     author = a;
     ISBN = id;
     availability = 1;
 }
+// Set up pointer getBookDetails as stated on Book.h
+void Book::displayBookDetails()
